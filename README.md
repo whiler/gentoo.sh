5236ec3861a9689637603686cc6de0463de835e4
whiler <wenwu500@qq.com>
2017-07-19 01:34:31 +0800

    parse arguments

--- README.md
+++ README.md
@@ -3,15 +3,15 @@
 install gentoo/linux quickly
 
     gentoo.sh \
-        --arch amd64|x86|arm \
-        --keywords amd64|x86|arm \
-        --platform generic|mbp|pi \
-        --mirrors http://mirrors.163.com/gentoo/ http://mirrors.sohu.com/gentoo/ \
-        --rsync rsync.cn.gentoo.org \
-        --stage3 /path/to/stage3 \
-        --portage /path/to/portage \
-        --firmware /path/to/firmware \
-        --hostname gentoo \
-        --timezone Asia/Shanghai \
-        --public-key /path/to/public.key \
-        --luks /path/to/dmcrypt.key
+        --arch=amd64|x86|arm \
+        --keywords=amd64|x86|arm \
+        --platform=generic|mbp|pi \
+        --mirrors=http://mirrors.163.com/gentoo/ http://mirrors.sohu.com/gentoo/ \
+        --rsync=rsync.cn.gentoo.org \
+        --stage3=/path/to/stage3 \
+        --portage=/path/to/portage \
+        --firmware=/path/to/firmware \
+        --hostname=gentoo \
+        --timezone=Asia/Shanghai \
+        --public-key=/path/to/public.key \
+        --luks=/path/to/dmcrypt.key
--- gentoo.sh
+++ gentoo.sh
@@ -1,1 +1,91 @@
 #!/bin/bash
+
+# {{{ arguments
+ARCH=
+KEYWORDS=
+PLATFORM=
+MIRRORS=
+RSYNC=
+STAGE3=
+PORTAGE=
+FIRMWARE=
+HOSTNAME=
+TIMEZONE=
+PUBLICKEY=
+LUKS=
+# }}}
+
+
+main() {
+    for arg in "${@}"
+    do
+        case "${arg}" in 
+            --arch=*)
+                ARCH="${arg#*=}"
+                shift
+                ;;
+
+            --keywords=*)
+                KEYWORDS="${arg#*=}"
+                shift
+                ;;
+
+            --platform=*)
+                PLATFORM="${arg#*=}"
+                shift
+                ;;
+
+            --mirrors=*)
+                MIRRORS="${arg#*=}"
+                shift
+                ;;
+
+            --rsync=*)
+                RSYNC="${arg#*=}"
+                shift
+                ;;
+
+            --stage3=*)
+                STAGE3="${arg#*=}"
+                shift
+                ;;
+
+            --portage=*)
+                PORTAGE="${arg#*=}"
+                shift
+                ;;
+
+            --firmware=*)
+                FIRMWARE="${arg#*=}"
+                shift
+                ;;
+
+            --hostname=*)
+                HOSTNAME="${arg#*=}"
+                shift
+                ;;
+
+            --timezone=*)
+                TIMEZONE="${arg#*=}"
+                shift
+                ;;
+
+            --public-key=*)
+                PUBLICKEY="${arg#*=}"
+                shift
+                ;;
+
+            --luks=*)
+                LUKS="${arg#*=}"
+                shift
+                ;;
+
+            *)
+                shift
+                ;;
+        esac
+
+    done
+}
+
+main "${@}"
