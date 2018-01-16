fd2c66723c89fbc1b4de709d8ac3d47fa42492e7
whiler <wenwu500@qq.com>
2018-01-16 10:05:40 +0000

    generic +swap +lvm +dmcrypt

---
+++ platforms/generic.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+source "${SCRIPT}/lib/log.sh"
+
+REQUIRED="${REQUIRED} cryptsetup"
+ENABLEDMCRYPT=Y
+ENABLESWAP=Y
+
+check-platform-arguments() {
+	local ret=0
+	if [[ -z "${DMCRYPTKEY}" ]]; then
+		LOGW "dmcrypt-key is required"
+		ret=1
+	fi
+	return ${ret}
+}
+
+init-platform() {
+	modprobe {dm-mod,dm-crypt,aes,sha256,cbc}
+	return $?
+}
