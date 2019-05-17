95c749847dcb038fab9e89f474b8c3c0d83e8996
whiler <wenwu500@qq.com>
2019-05-17 14:05:31 +0800

    fetch raspberrypi firmware and uniform it

---
+++ scripts/fetch.pi.kernel.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+dest="${1}"
+outdir="${dest:="."}"
+repo="raspberrypi/firmware"
+latestTag="$(curl --silent "https://api.github.com/repos/${repo}/tags" | grep "name" | head --lines=1 | cut --delimiter=\" --fields=4)"
+if [[ ! -z "${latestTag}" ]]; then
+	url="https://github.com/${repo}/archive/${latestTag}.tar.gz"
+	filename="${outdir}/pi.kernel.${latestTag}.tar.gz"
+	echo "fetching ${url} to ${filename}"
+	curl --silent --location --output "${filename}" ${url}
+
+	tmpdir="$(mktemp --directory)"
+	tar -xzf "${filename}" -C "${tmpdir}"
+	rm "${filename}"
+	pushd "${tmpdir}/firmware-${latestTag}"
+		rm  README.md
+		rm -r documentation
+		mv extra/* boot/
+		rm -r extra
+		mkdir -p lib
+		mv modules lib
+	popd
+	tar -czf "${filename}" -C "${tmpdir}/firmware-${latestTag}" .
+	rm -r "${tmpdir}"
+else
+	echo "get latest tag failed"
+fi
