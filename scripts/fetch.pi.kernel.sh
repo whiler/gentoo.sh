f3de4a45cedf7f9ce2f508ca2829b6c9785454cb
whiler <wenwu500@qq.com>
2019-05-27 02:55:50 +0800

    check tar.gz

--- scripts/fetch.pi.kernel.sh
+++ scripts/fetch.pi.kernel.sh
@@ -8,7 +8,10 @@ if [[ ! -z "${latestTag}" ]]; then
 	url="https://github.com/${repo}/archive/${latestTag}.tar.gz"
 	filename="${outdir}/pi.kernel.${latestTag}.tar.gz"
 	echo "fetching ${url} to ${filename}"
-	curl --silent --location --output "${filename}" ${url}
+	until test -e "${filename}" && tar -tf "${filename}" > /dev/null 2>&1; do
+		rm -f "${filename}"
+		curl --silent --location --output "${filename}" ${url}
+	done
 
 	tmpdir="$(mktemp --directory)"
 	tar -xzf "${filename}" -C "${tmpdir}"
